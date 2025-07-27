cf4fbc86e25090ebd407af4d8698dc4628ecfae5
Mox <eleanorlambsongbird@gmail.com>
2025-07-26 23:01:07 -0300

    Added Ground Check Collision;

--- Assets/Scripts/Skyscraper/Player/MovementHandler.cs
+++ Assets/Scripts/Skyscraper/Player/MovementHandler.cs
@@ -4,23 +4,60 @@ using UnityEngine;
 
 public class MovementHandler : MonoBehaviour
 {
+    public Rigidbody2D rigidB;
+
+    [Header("Movement Info")]
     public float moveSpeed;
     public float jumpForce;
-    public Rigidbody2D rigidB;
+    
+
+    [Header("Collision Info")]
+    public float groundCheckDistance;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        rigidB.velocity = new Vector2(moveSpeed, rigidB.velocity.y);
+        Move();
+        Jump();
+    }
 
-        if(Input.GetButtonDown("Jump"))
-        { 
+    private void FixedUpdate()
+    {
+        CheckCollision();
+    }
+
+    #region Move
+    void Move()
+    {
+            rigidB.velocity = new Vector2(moveSpeed, rigidB.velocity.y);
+    }
+    #endregion
+
+    #region Jump
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
             rigidB.velocity = new Vector2(rigidB.velocity.x, jumpForce);
         }
     }
+    #endregion
+
+    #region Collision
+    void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+    #endregion
 }
 
